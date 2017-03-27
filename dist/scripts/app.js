624e86098153cc353789cf4c190d39d9358f9804
Azucena Martinez <azucena_rys@yahoo.com>
2017-03-27 14:02:42 -0600

    assignment-6

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,13 +1,19 @@
+//Here is an empty function
 (function() {
+  // router
+  // This is a function with two arguments
   function config($stateProvider, $locationProvider) {
-    $locationProvider
+    // It looks like these arguments have objects inside of them.
+    $locationProvider //not sure how this works here
       .html5Mode({
-        enabled:true,
+        enabled:true,  // key value pairs
         requireBase: false
       });
     $stateProvider
+    //  .something means a method??
       .state('landing', {
         url: '/',
+        controller: 'LandingCtrl as landing',
         templateUrl: '/templates/landing.html'
       })
       .state('album', {
@@ -16,6 +22,7 @@
       })
       .state('collection', {
         url: '/collection',
+        controller: 'CollectionCtrl as collection',
         templateUrl: '/templates/collection.html'
       });
   }
