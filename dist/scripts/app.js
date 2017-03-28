5ba8cb97879f288f8e146a1ff9d627ef132e5b97
Azucena Martinez <azucena_rys@yahoo.com>
2017-03-28 10:36:58 -0600

    merge fixes

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,21 +1,29 @@
+//Here is an empty function
 (function() {
+  // router
+  // This is a function with two arguments
   function config($stateProvider, $locationProvider) {
-    $locationProvider
+    // It looks like these arguments have objects inside of them.
+    $locationProvider //not sure how this works here
       .html5Mode({
-        enabled:true,
+        enabled:true,  // key value pairs
         requireBase: false
       });
     $stateProvider
+    //  .something means a method??
       .state('landing', {
         url: '/',
+        controller: 'LandingCtrl as landing',
         templateUrl: '/templates/landing.html'
       })
       .state('album', {
         url: '/album',
+        controller: 'AlbumCtrl as album',
         templateUrl: '/templates/album.html'
       })
       .state('collection', {
         url: '/collection',
+        controller: 'CollectionCtrl as collection',
         templateUrl: '/templates/collection.html'
       });
   }
