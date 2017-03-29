6ed6af5bd06693d7643284bf2e476e3f256e1a18
Azucena Martinez <azucena_rys@yahoo.com>
2017-03-29 16:20:42 -0600

    playSong function

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,22 +1,40 @@
 (function() {//This is the SongPlayer service
   function SongPlayer() {
+    // @desc private attribute for our SongPlayer service
+    // @type object, array, number, boolean, null? = object
     var SongPlayer = {};
-    var currentSong = null;//private attrubute for our SongPlayer service
-/*  @desc Buzz object audio file
-*@type 
-  */
-    var currentBuzzObject = null;//private attribute for our SongPlayer service
+    // @desc private attribute for our SongPlayer service
+    // @type object, array, number, boolean, null? = null
+    var currentSong = null;
+
+  /*
+ * @desc Buzz object audio file
+ * @type {Object}
+ */
+    var currentBuzzObject = null;
     /*
-     * @function setSong
-     * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+     * @function playSong
+     * @desc This is a function that has a method of play and a method with a boolean?
      * @param {Object} song
      * @returns {Number}
      */
 
+    var playSong = function(){
+
+      currentBuzzObject.play();
+      song.playing = true;
+
+    };
     var setSong = function(song) {//this is a private function(setSong)
       if (currentBuzzObject) {
         currentBuzzObject.stop();
         currentSong.playing = null;
+        /*
+         * @function setSong
+         * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+         * @param {Object} song
+         * @returns {Number}
+         */
       }
       currentBuzzObject = new buzz.sound(song.audioUrl, {
         formats: ['mp3'],
@@ -24,11 +42,13 @@
       });
       currentSong = song;
     };
+
     SongPlayer.play = function(song) {//public method
       if (currentSong !== song) {
         setSong(song);
-        currentBuzzObject.play();
-        song.playing = true;
+        playSong();
+        /*currentBuzzObject.play();
+        song.playing = true; */
       } else if (currentSong === song) {
         if (currentBuzzObject.isPaused()) {
           currentBuzzObject.play();
